c504b5ccdd5dc8c435c01aa10dcfabb369ea4b7b
areebamalik <areebamalik031877>
2024-04-16 00:05:08 +0500

    push my currency converter

---
+++ index.ts
@@ -0,0 +1,39 @@
+import inquirer from "inquirer"
+const Currency:any = {
+    USD: 1, //base currency
+    EUR: 0.91,
+    GBP: 0.78,
+    INR: 74.57,
+    PKR: 280,
+};
+let user_Currency = await inquirer.prompt(
+ [
+    {
+        
+      name: "from",
+      message: "Enter from Currency",
+      type: 'list',
+      choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },     
+    {
+      name: 'to',
+      message: "Enter from Currency",
+      type: 'list',
+      choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+      name: "amount",
+      message: "Enter Your Amount",
+      type: "confirm",
+    }
+ ] 
+)
+let fromAmount = Currency[user_Currency.from]
+let toAmount = Currency[user_Currency.to]
+let amount = user_Currency.amount
+let baseAmount = amount / fromAmount // USD base currency // 4
+let convertedAmount = baseAmount * toAmount
+console.log(convertedAmount);
+console.log(fromAmount);
+console.log(toAmount);
+console.log(amount);
